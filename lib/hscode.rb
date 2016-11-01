59deaf3595b54dfe2d1ca8ec38050b488ce84623
Austin Kabiru <austin.kabiru@andela.com>
2016-11-01 17:40:08 +0300

    Abstract parser logic

--- lib/hscode.rb
+++ lib/hscode.rb
@@ -1,45 +1,15 @@
 require "hscode/version"
+require "hscode/input_parser"
 require "optparse"
 require "ostruct"
+require "pp"
 
 module Hscode
-  class InputParser
-    #
-    # Return a structure describing options
-    #
-    def self.parse(args)
-      # Default options
-      options = OpenStruct.new
-      options.verbose = false
-
-      opt_parser = OptionParser.new do |parser|
-        parser.banner = "Usage: hscode [options]"
-
-        parser.separator ""
-        parser.separator "Specific options:"
-
-        parser.on("-s", "--status [CODE]", Integer,
-          "Show HTTP status code documentation") do |code|
-            puts code
-            exit
-        end
-
-        parser.separator ""
-        parser.separator "Common options:"
-
-        parser.on_tail("-h", "--help", "Show this help message") do
-          puts parser
-          exit
-        end
-
-        parser.on_tail("--version", "Show version") do
-          puts ::VERSION
-          exit
-        end
-      end
-
-      opt_parser.parse!(args)
-      options
-    end # parser()
-  end # class OptionsParser
+  class CliController
+    def call(args)
+      options = Hscode::InputParser.parse(args)
+      pp options
+      pp args
+    end
+  end
 end
---
+++ lib/hscode/input_parser.rb
@@ -0,0 +1,42 @@
+module Hscode
+   class InputParser
+    #
+    # Return a structure describing options
+    #
+    def self.parse(args)
+      # Default options
+      options = OpenStruct.new
+      options.verbose = false
+      options.code = "200"
+
+      opt_parser = OptionParser.new do |parser|
+        parser.banner = "Usage: hscode [options]"
+
+        parser.separator ""
+        parser.separator "Specific options:"
+
+        parser.on("-c", "--code [CODE]", Integer,
+          "Show HTTP status code documentation") do |code|
+            puts code
+            exit
+        end
+
+        parser.separator ""
+        parser.separator "Common options:"
+
+        parser.on_tail("-h", "--help", "Show this help message") do
+          puts parser
+          exit
+        end
+
+        parser.on_tail("--version", "Show version") do
+          puts ::VERSION
+          exit
+        end
+      end
+
+      opt_parser.parse!(args)
+      options
+    end # parser()
+  end # class OptionsParser
+end
