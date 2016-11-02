4d6acc7aba237044d16fa1d1288ba21a07c127fe
Herbert Kagumba <herbert.kagumba@andela.com>
2016-11-02 19:29:23 +0300

    Chore - Setup testing suite.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,25 @@
+require 'simplecov'
+require 'coveralls'
+
+Coveralls.wear!
+
+SimpleCov.formatters = [
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+SimpleCov.start
+
+require_relative '../config/bootstrap.rb'
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+spec_dir = File.join($app_root, 'spec')
+$LOAD_PATH.unshift(spec_dir) unless $LOAD_PATH.include?(spec_dir)
+
 require 'hscode'
+require 'rspec'
+
+RSpec.configure do |config|
+  # custom RSpec configurations
+end
+
+ENV["RUBY_ENV"] = "test"
