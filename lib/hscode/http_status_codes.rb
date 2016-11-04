19e67fca01e2c3b7aba53cb4a50c0784680bcba2
Austin Kabiru <austin.kabiru@andela.com>
2016-11-04 17:04:26 +0300

    [Feature] add status code documentation

--- lib/hscode/http_status_codes.rb
+++ lib/hscode/http_status_codes.rb
@@ -1,5 +1,3 @@
-require 'hscode/storage_hash'
-
 module Hscode
   HTTP_STATUS_CODES = {
     100 => {
@@ -497,6 +495,398 @@ module Hscode
         This specification defines no equivalent counterpart for 301 \
         Moved Permanently (RFC7238, however, proposes defining the status \
         code 308 Permanent Redirect for this purpose)."
+      ]
+    },
+    308 => {
+      title: 'Permanent Redirect',
+      description: [
+        "The target resource has been assigned a new permanent URI and \
+        any future references to this resource ought to use one of the \
+        enclosed URIs.",
+        "Clients with link editing capabilities ought to automatically \
+        re-link references to the effective request URI to one or more \
+        of the new references sent by the server, where possible.",
+        "The server SHOULD generate a Location header field2 in the \
+        response containing a preferred URI reference for the new \
+        permanent URI. The user agent MAY use the Location field value \
+        for automatic redirection. The server's response payload usually \
+        contains a short hypertext note with a hyperlink to \
+        the new URI(s).",
+        "A 308 response is cacheable by default; i.e., unless otherwise \
+        indicated by the method definition or explicit cache controls3.",
+        "Note: This status code is similar to 301 Moved Permanently, \
+        except that it does not allow changing the request method \
+        from POST to GET."
+      ]
+    },
+    400 => {
+      title: 'Bad Request',
+      description: [
+        "The server cannot or will not process the request due to \
+        something that is perceived to be a client error \
+        (e.g., malformed request syntax, invalid request message \
+        framing, or deceptive request routing)."
+      ]
+    },
+    401 => {
+      title: 'Unauthorized',
+      description: [
+        "The request has not been applied because it lacks valid \
+        authentication credentials for the target resource.",
+        "The server generating a 401 response MUST send a \
+        WWW-Authenticate header field containing at least one challenge \
+        applicable to the target resource.",
+        "If the request included authentication credentials, then the \
+        401 response indicates that authorization has been refused for \
+        those credentials. The user agent MAY repeat the request with a \
+        new or replaced Authorization header field2. If the 401 response \
+        contains the same challenge as the prior response, and the user \
+        agent has already attempted authentication at least once, then \
+        the user agent SHOULD present the enclosed representation to the \
+        user, since it usually contains relevant diagnostic information."
+      ]
+    },
+    402 => {
+      title: 'Payment Required',
+      description: ['Reserved for future use.']
+    },
+    403 => {
+      title: 'Forbidden',
+      description: [
+        'The server understood the request but refuses to authorize it.',
+        "A server that wishes to make public why the request has been \
+        forbidden can describe that reason in the response \
+        payload (if any).",
+        "If authentication credentials were provided in the request, \
+        the server considers them insufficient to grant access. The \
+        client SHOULD NOT automatically repeat the request with the \
+        same credentials. The client MAY repeat the request with new \
+        or different credentials. However, a request might be forbidden \
+        for reasons unrelated to the credentials.",
+        "An origin server that wishes to \"hide\" the current \
+        existence of a forbidden target resource MAY instead respond \
+        with a status code of 404 Not Found."
+      ]
+    },
+    404 => {
+      title: 'Not Found',
+      description: [
+        "The origin server did not find a current representation for \
+        the target resource or is not willing to disclose \
+        that one exists.",
+        "A 404 status code does not indicate whether this lack of \
+        representation is temporary or permanent; the 410 Gone \
+        status code is preferred over 404 if the origin server knows, \
+        presumably through some configurable means, that the condition \
+        is likely to be permanent.",
+        "A 404 response is cacheable by default; i.e., unless otherwise \
+        indicated by the method definition or explicit cache controls."
+      ]
+    },
+    405 => {
+      title: 'Method Not Allowed',
+      description: [
+        "The method received in the request-line is known by the \
+        origin server but not supported by the target resource.",
+        "The origin server MUST generate an Allow header field in a \
+        405 response containing a list of the target resource's \
+        currently supported methods.",
+        "A 405 response is cacheable by default; i.e., unless \
+        otherwise indicated by the method definition or explicit \
+        cache controls."
+      ]
+    },
+    406 => {
+      title: 'Not Acceptable',
+      description: [
+        "The target resource does not have a current representation \
+        that would be acceptable to the user agent, according to \
+        the proactive negotiation header fields received in the \
+        request, and the server is unwilling to supply a default \
+        representation.",
+        "The server SHOULD generate a payload containing a list of \
+        available representation characteristics and corresponding \
+        resource identifiers from which the user or user agent can \
+        choose the one most appropriate. A user agent MAY automatically \
+        select the most appropriate choice from that list. However, \
+        this specification does not define any standard for such \
+        automatic selection, as described in RFC7231 Section 6.4.1."
+      ]
+    },
+    407 => {
+      title: 'Proxy Authentication Required',
+      description: [
+        "Similar to 401 Unauthorized, but it indicates that the \
+        client needs to authenticate itself in order to use a proxy.",
+        "The proxy MUST send a Proxy-Authenticate header field \
+        containing a challenge applicable to that proxy for the target \
+        resource. The client MAY repeat the request with a new or \
+        replaced Proxy-Authorization header field2."
+      ]
+    },
+    408 => {
+      title: 'Request Timeout',
+      description: [
+        "The server did not receive a complete request message within \
+        the time that it was prepared to wait.",
+        "A server SHOULD send the \"close\" connection option in the \
+        response, since 408 implies that the server has decided to \
+        close the connection rather than continue waiting. If the \
+        client has an outstanding request in transit, the client MAY \
+        repeat that request on a new connection."
+      ]
+    },
+    409 => {
+      title: 'Conflict',
+      description: [
+        "The request could not be completed due to a conflict with \
+        the current state of the target resource. This code is used \
+        in situations where the user might be able to resolve the \
+        conflict and resubmit the request.",
+        "The server SHOULD generate a payload that includes enough \
+        information for a user to recognize the source of the conflict.",
+        "Conflicts are most likely to occur in response to a PUT \
+        request. For example, if versioning were being used and the \
+        representation being PUT included changes to a resource that \
+        conflict with those made by an earlier (third-party) request, \
+        the origin server might use a 409 response to indicate that it \
+        can't complete the request. In this case, the response \
+        representation would likely contain information useful for \
+        merging the differences based on the revision history."
+      ]
+    },
+    410 => {
+      title: 'Gone',
+      description: [
+        "The target resource is no longer available at the origin \
+        server and that this condition is likely to be permanent.",
+        "If the origin server does not know, or has no facility to \
+        determine, whether or not the condition is permanent, the \
+        status code 404 Not Found ought to be used instead.",
+        "The 410 response is primarily intended to assist the task of \
+        web maintenance by notifying the recipient that the resource is \
+        intentionally unavailable and that the server owners desire that \
+        remote links to that resource be removed. Such an event is \
+        common for limited-time, promotional services and for resources \
+        belonging to individuals no longer associated with the origin \
+        server's site. It is not necessary to mark all permanently \
+        unavailable resources as \"gone\" or to keep the mark for any \
+        length of time -- that is left to the discretion of the \
+        server owner.",
+        "A 410 response is cacheable by default; i.e., unless \
+        otherwise indicated by the method definition or explicit \
+        cache controls."
+      ]
+    },
+    411 => {
+      title: 'Length Required',
+      description: [
+        "The server refuses to accept the request without a defined \
+        Content-Length.",
+        "The client MAY repeat the request if it adds a valid \
+        Content-Length header field containing the length of the \
+        message body in the request message."
+      ]
+    },
+    412 => {
+      title: 'Precondition Failed',
+      description: [
+        "One or more conditions given in the request header fields \
+        evaluated to false when tested on the server.",
+        "This response code allows the client to place preconditions \
+        on the current resource state (its current representations and \
+        metadata) and, thus, prevent the request method from being \
+        applied if the target resource is in an unexpected state."
+      ]
+    },
+    413 => {
+      title: 'Payload Too Large',
+      description: [
+        "The server is refusing to process a request because the request \
+        payload is larger than the server is willing or able to process.",
+        "The server MAY close the connection to prevent the client from \
+        continuing the request.",
+        "If the condition is temporary, the server SHOULD generate a \
+        Retry-After header field to indicate that it is temporary and \
+        after what time the client MAY try again."
+      ]
+    },
+    414 => {
+      title: 'Request-URI Too Long',
+      description: [
+        "The server is refusing to service the request because \
+        the request-target is longer than the server is willing \
+        to interpret.",
+        "This rare condition is only likely to occur when a client \
+        has improperly converted a POST request to a GET request with \
+        long query information, when the client has descended into \
+        a \"black hole\" of redirection (e.g., a redirected URI prefix \
+        that points to a suffix of itself) or when the server is under \
+        attack by a client attempting to exploit potential \
+        security holes.", "A 414 response is cacheable by default; i.e., \
+            unless otherwise indicated by the method definition or explicit \
+            cache controls 2."
+      ]
+    },
+    415 => {
+      title: 'Unsupported Media Type',
+      description: [
+        "The origin server is refusing to service the request because \
+        the payload is in a format not supported by this method on \
+        the target resource.",
+        "The format problem might be due to the request's indicated \
+        Content-Type or Content-Encoding, or as a result of inspecting \
+        the data directly."
+      ]
+    },
+    416 => {
+      title: 'Requested Range Not Satisfiable',
+      description: [
+        "None of the ranges in the request's Range header field overlap \
+        the current extent of the selected resource or that the set of \
+        ranges requested has been rejected due to invalid ranges or an \
+        excessive request of small or overlapping ranges.",
+        "For byte ranges, failing to overlap the current extent means \
+        that the first-byte-pos of all of the byte-range-spec values \
+        were greater than the current length of the selected \
+        representation. When this status code is generated in response \
+        to a byte-range request, the sender SHOULD generate a \
+        Content-Range header field specifying the current length of \
+        the selected representation2.",
+        'For example:',
+        "Note: Because servers are free to ignore Range, many \
+        implementations will simply respond with the entire selected \
+        representation in a 200 OK response. That is partly because \
+        most clients are prepared to receive a 200 OK to complete the \
+        task (albeit less efficiently) and partly because clients might \
+        not stop making an invalid partial request until they have \
+        received a complete representation. Thus, clients cannot depend \
+        on receiving a 416 Range Not Satisfiable response even when it \
+        is most appropriate."
+      ]
+    },
+    417 => {
+      title: 'Expectation Failed',
+      description: [
+        "The expectation given in the request's Expect header field \
+        could not be met by at least one of the inbound servers."
+      ]
+    },
+    418 => {
+      title: "I'm a teapot",
+      description: [
+        "Any attempt to brew coffee with a teapot should result in the \
+        error code \"418 I'm a teapot\". The resulting entity body \
+        MAY be short and stout."
+      ]
+    },
+    421 => {
+      title: 'Misdirected Request',
+      description: [
+        "The request was directed at a server that is not able to produce a \
+        response. This can be sent by a server that is not configured to \
+        produce responses for the combination of scheme and authority that \
+        are included in the request URI.",
+        "Clients receiving a 421 Misdirected Request response from a server \
+        MAY retry the request -- whether the request method is idempotent \
+        or not -- over a different connection. This is possible if a \
+        connection is reusedor if an alternative service is selected ALT-SVC.",
+        'This status code MUST NOT be generated by proxies.',
+        "A 421 response is cacheable by default, i.e., unless otherwise \
+        indicated by the method definition or explicit cache controls2."
+      ]
+    },
+    422 => {
+      title: 'Unprocessable Entity',
+      description: [
+        "The server understands the content type of the request \
+        entity (hence a 415 Unsupported Media Type status code is \
+        inappropriate), and the syntax of the request entity is \
+        correct (thus a 400 Bad Request status code is inappropriate) \
+        but was unable to process the contained instructions.",
+        "For example, this error condition may occur if an XML request \
+        body contains well-formed (i.e., syntactically correct), but \
+        semantically erroneous, XML instructions."
+      ]
+    },
+    423 => {
+      title: 'Locked',
+      description: [
+        'The source or destination resource of a method is locked.',
+        "This response SHOULD contain an appropriate precondition or \
+        postcondition code, such as 'lock-token-submitted' \
+        or 'no-conflicting-lock'."
+      ]
+    },
+    424 => {
+      title: 'Failed Dependency',
+      description: [
+        "The method could not be performed on the resource because the \
+        requested action depended on another action and that \
+        action failed.",
+        "For example, if a command in a PROPPATCH method fails, then, \
+        at minimum, the rest of the commands will also fail with 424 \
+        Failed Dependency."
+      ]
+    },
+    426 => {
+      title: 'Upgrade Required',
+      description: [
+        "The server refuses to perform the request using the current \
+        protocol but might be willing to do so after the client upgrades \
+        to a different protocol.",
+        "The server MUST send an Upgrade header field in a 426 response \
+        to indicate the required protocol(s)",
+        'Example:'
+      ]
+    },
+    428 => {
+      title: 'Precondition Required',
+      description: [
+        'The origin server requires the request to be conditional.',
+        "Its typical use is to avoid the \"lost update\" problem, where \
+        a client GETs a resource's state, modifies it, and PUTs it back \
+        to the server, when meanwhile a third party has modified the \
+        state on the server, leading to a conflict. By requiring requests \
+        to be conditional, the server can assure that clients are working \
+        with the correct copies.",
+        "Responses using this status code SHOULD explain how to resubmit \
+        the request successfully. For example:",
+        'Responses with the 428 status code MUST NOT be stored by a cache.'
+      ]
+    },
+    429 => {
+      title: 'Too Many Requests',
+      description: [
+        "The user has sent too many requests in a given amount of time \
+        (\"rate limiting\").", "The response representations SHOULD \
+            include details explaining the condition, and MAY include a \
+            Retry-After header indicating how long to wait before making a \
+            new request.",
+        'For example:',
+        "Note that this specification does not define how the origin \
+        server identifies the user, nor how it counts requests. For \
+        example, an origin server that is limiting request rates can do \
+        so based upon counts of requests on a per-resource basis, \
+        across the entire server, or even among a set of servers. \
+        Likewise, it might identify the user by its authentication \
+        credentials, or a stateful cookie.",
+        "Responses with the 429 status code MUST NOT be \
+        stored by a cache."
+      ]
+    },
+    431 => {
+      title: 'Request Header Fields Too Large',
+      description: [
+        "The server is unwilling to process the request because its \
+        header fields are too large. The request MAY be resubmitted \
+        after reducing the size of the request header fields.",
+        "It can be used both when the set of request header fields in \
+        total is too large, and when a single header field is at fault.  \
+        In the latter case, the response representation SHOULD specify \
+        which header field was too large.",
+        'For example:',
+        'Responses with the 431 status code MUST NOT be stored by a cache.'
       ]
     }
   }.freeze
