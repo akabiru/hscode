878dac130cfe517d63045ecade976c70c0c1bf8a
Austin Kabiru <austin.kabiru@andela.com>
2016-11-11 10:47:08 +0300

    add cli controller specs

--- spec/hscode_spec.rb
+++ spec/hscode_spec.rb
@@ -4,4 +4,30 @@ describe Hscode do
   it 'has a version number' do
     expect(Hscode::VERSION).not_to be nil
   end
+
+  describe Hscode::CliController do
+    it 'responds to .call' do
+      expect(Hscode::CliController).to respond_to(:call)
+    end
+
+    describe '.call' do
+      context 'when options is verbose' do
+        let(:verbose_cmd) { described_class.call(['-c', '200', '-v']) }
+
+        it 'prints full status code documentation' do
+          expect(verbose_cmd).to be_an_instance_of(Array)
+          expect(verbose_cmd)
+            .to match_array(Hscode::HTTP_STATUS_CODES[200][:description])
+        end
+      end
+
+      context 'when options is non verbose' do
+        let(:non_verbose_cmd) { described_class.call(['-c', '422']) }
+
+        it 'prints code title' do
+          expect(non_verbose_cmd).to be_nil
+        end
+      end
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 require 'coveralls'
+require 'pry'
 
 Coveralls.wear!
 
