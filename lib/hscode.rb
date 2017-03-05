c3248aac85d706dad1cc38d3527ae104ccb0ee8e
Adepoju Adebayo <mradeybee@gmail.com>
2017-03-05 11:39:24 +0100

    add search functionality

--- lib/hscode.rb
+++ lib/hscode.rb
@@ -1,6 +1,7 @@
 require 'hscode/version'
 require 'hscode/input_parser'
 require 'hscode/http_status_codes'
+require 'hscode/pretty_print'
 require 'optparse'
 require 'ostruct'
 
@@ -8,13 +9,13 @@ module Hscode
   class CliController
     def self.call(args)
       options = Hscode::InputParser.new.parse(args)
-      print_code(options)
+      options.title ? print_title_code(options) : print_code(options)
     end
 
     private_class_method
 
     def self.print_code(options)
-      status_code = HTTP_STATUS_CODES[options.status_code.to_i]
+      status_code =  HTTP_STATUS_CODES[options.status_code.to_i]
 
       unless status_code
         puts "#{options.status_code} is not a valid code. See 'hscode --help'."
@@ -26,6 +27,24 @@ module Hscode
         options.status_code.to_s[0]
       )
       print_description(status_code) if options.verbose
+    end
+
+    def self.print_title_code(options)
+      status_object =  HTTP_STATUS_CODES.detect {|_, value| value[:title].downcase == options.title.downcase}
+
+      unless status_object
+        puts "#{options.title} is not a valid HTTP status. See 'hscode --list' to see the list of valid HTTP codes."
+        exit
+      end
+
+      status_code = status_object.first
+
+       PrettyPrint.print(
+        "#{status_object[1][:title]} - #{status_code}",
+        status_code.to_s[0]
+      )
+
+      print_description(status_object[1]) if options.verbose
     end
 
     def self.print_description(status_code)
